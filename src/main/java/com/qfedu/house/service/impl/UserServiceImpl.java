a268c8edcfddf1271373f565d60115da317b1983
790013438 <790013438@qq.com>
2017-10-18 11:28:51 +0800

    spring mvc

--- src/main/java/com/qfedu/house/service/impl/UserServiceImpl.java
+++ src/main/java/com/qfedu/house/service/impl/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.qfedu.house.service.impl;
 
+import java.util.Date;
+
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.qfedu.house.domain.LoginLog;
 import com.qfedu.house.domain.User;
 import com.qfedu.house.persistence.LoginLogDao;
 import com.qfedu.house.persistence.UserDao;
@@ -17,16 +21,32 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private LoginLogDao loginLogDao;
 	
-	
 	@Override
 	public boolean login(User user) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean flag = false;
+		User temp = userDao.findByUsername(user.getUsername());
+		if (temp != null) {
+			String md5 = DigestUtils.md5Hex(user.getPassword());
+			flag = temp.getPassword().equals(md5);
+			if (flag) {
+				LoginLog loginLog = new LoginLog();
+				loginLog.setUser(temp);
+				loginLog.setIpAddress("211.135.62.90");
+				loginLog.setLogDate(new Date());
+				loginLogDao.save(loginLog);
+			}
+		}
+		return flag;
 	}
 
 	@Override
 	public boolean register(User user) {
-		// TODO Auto-generated method stub
+		User temp = userDao.findByUsername(user.getUsername());
+		if (temp == null) {
+			String md5 = DigestUtils.md5Hex(user.getPassword());
+			user.setPassword(md5);
+			return userDao.save(user) != null;
+		}
 		return false;
 	}
 
